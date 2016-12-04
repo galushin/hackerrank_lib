8a0d2da945d4de12957583d2d451c03646f71aef
Pavel Galushin <galushin@users.noreply.github.com>
2016-12-04 13:13:07 +0700

    Решето Эратосфена

--- all.hpp
+++ all.hpp
@@ -5,3 +5,32 @@ T read(IStream & is)
     is >> x;
     return x;
 }
+
+/**
+@todo Пропускать чётные
+*/
+template <class IntType>
+vector<IntType> make_primes_below(IntType p_max)
+{
+    vector<IntType> primes;
+    
+    {
+        vector<bool> seive(p_max, true);
+        
+        for(auto i = seive.begin(); i != seive.end(); i = find(i, seive.end(), true))
+        {
+            auto const p = (i - seive.begin()) + 2;
+
+            primes.push_back(p);
+
+            for(auto j = p - 2; j < seive.size(); j += p)
+            {
+                seive[j] = false;
+            }
+
+            ++ i;
+        }
+    }
+    
+    return primes;
+}
